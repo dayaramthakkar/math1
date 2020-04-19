7b0850ce9512b49cbd62ed913096214cbc0835ab
m0ntu8024-out <m0ntu8024@outlook.com>
2020-04-19 18:23:43 +0530

    css change

--- js/main.js
+++ js/main.js
@@ -19,25 +19,45 @@ function clear() {
 function calculate() {
     var n = parseInt(inputselect.val());
     table = $('<table>');
+
     for (var k = 0; k <= n; k++) {
         var row = $('<tr>');
         table.append(row);
+
+        var td = $('<td>').addClass('td-k');
+        row.append(td);
+        if (k == 0) {
+            td.addClass('td-k-first').html('i  /  j');
+        }
+        else {
+            td.html(k);
+        }
+
         for (var r = 0; r <= n; r++) {
             var td = $('<td>');
             row.append(td);
-            if (r === k) {
-                td.addClass('td-rk');
-            }
             answer = 0;
-            if (r >= k) {
-                // 2^r
-                answer = Math.pow(2, r);
-
+            if (k === 0) {
+                answer = r;
+                td.addClass('td-r');
             }
             else {
-                // 2^r(n*(2^(n-k) + 1 ))
-                answer = Math.pow(2, r) * (n * (Math.pow(2, n - k) ) + 1);
+
+                if (r === k) {
+                    td.addClass('td-rk');
+                }
+
+                if (r >= k) {
+                    // 2^r
+                    answer = Math.pow(2, r);
+
+                }
+                else {
+                    // 2^r(n*(2^(n-k) + 1 ))
+                    answer = Math.pow(2, r) * (n * (Math.pow(2, n - k)) + 1);
+                }
             }
+
             td.html(answer)
         }
     }
